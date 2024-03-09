7580fc1384957ebe104ea642a21d22fca0fdaa4c
Viral Vaghela <viralvaghela8577@gmail.com>
2024-03-09 22:07:28 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="text-center fw-bold fs-1">
+      Indian Life Simulator
+    </div>
+    </>
+  )
+}
+
+export default App
