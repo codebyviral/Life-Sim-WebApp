1ed1a435e5ba65e7951738484e530b769a866e4e
Viral Vaghela <viralvaghela8577@gmail.com>
2024-03-10 15:37:06 +0530

    style - update

--- frontend/src/Components/PlayerNav.jsx
+++ frontend/src/Components/PlayerNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const PlayerNav = () => {
+  const age = 18;
+  const [day, setDay] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (day < 31) {
+        setDay((day) => day + 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  if(day === 32){
+    setDay(1)
+  }
   return (
-    <div>PlayerNav</div>
-  )
-}
+    <>
+      <div className="ageNav d-flex">
+        <span>{`${age} years`}</span>
+        
+        <span>{day}</span>
+      </div>
+    </>
+  );
+};
 
-export default PlayerNav
+export default PlayerNav;
--- frontend/src/Pages/StartGame.jsx
+++ frontend/src/Pages/StartGame.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LifeNav from "../Components/LifeNav"
+import PlayerNav from '../Components/PlayerNav'
 
 const StartGame = () => {
   return (
     <>
     <LifeNav />
+    <PlayerNav />
     </>
   )
 }
